fc8b344d03a40fe1995c767b71f09da02b90c827
VanHelsing2048 <75938319+VanHelsing2048@users.noreply.github.com>
2025-06-04 22:52:52 +0200

    Add tests for utility and device handling

---
+++ tests/MQTT_NET_COMELIT.Tests/MQTTHomeAssistantTests.cs
@@ -0,0 +1,32 @@
+using System.Reflection;
+using System.Runtime.Serialization;
+using MQTT_NET_COMELIT.Comelit;
+using MQTT_NET_COMELIT.Comelit.DevicesStructure;
+using MQTT_NET_COMELIT.HomeAssistant;
+using Xunit;
+using System.Collections.Generic;
+
+namespace MQTT_NET_COMELIT.Tests
+{
+    public class MQTTHomeAssistantTests
+    {
+        [Fact]
+        public void GetDeviceFromTopic_ReturnsCorrectDevice()
+        {
+            var device = new DigitalLight { ID = "room#light.1" };
+            var area = new Area { Devices = new List<Device> { device } };
+            var home = new Home { Areas = new List<Area> { area } };
+
+            var comelit = (MQTTComelit)FormatterServices.GetUninitializedObject(typeof(MQTTComelit));
+            typeof(MQTTComelit).GetField("<HomeStructure>k__BackingField", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(comelit, home);
+
+            var ha = (MQTTHomeAssistant)FormatterServices.GetUninitializedObject(typeof(MQTTHomeAssistant));
+            ha.MQTTComelit = comelit;
+
+            var method = typeof(MQTTHomeAssistant).GetMethod("GetDeviceFromTopic", BindingFlags.Instance | BindingFlags.NonPublic);
+            var result = (Device)method.Invoke(ha, new object[] { $"home/lights/{device.GetIDForTopic()}/set" });
+
+            Assert.Same(device, result);
+        }
+    }
+}
